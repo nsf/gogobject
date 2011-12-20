b56e8c5467235920d44ed5585005df8e7d650d2b
nsf <no.smile.face@gmail.com>
2011-12-20 17:54:12 +0600

    Manually implement ParamSpec hierarchy. Tweak few things.

--- gobject-2.0/gobject.c
+++ gobject-2.0/gobject.c
@@ -8,6 +8,14 @@ struct _GGoClosure {
 	void *iface[2];
 };
 
+GType _g_param_spec_type(GParamSpec *pspec) {
+	return G_PARAM_SPEC_TYPE(pspec);
+}
+
+GType _g_param_spec_value_type(GParamSpec *pspec) {
+	return G_PARAM_SPEC_VALUE_TYPE(pspec);
+}
+
 GType _g_object_type(GObject *obj) {
 	return G_OBJECT_TYPE(obj);
 }
@@ -16,27 +24,56 @@ GType _g_value_type(GValue *val) {
 	return G_VALUE_TYPE(val);
 }
 
-GType _g_type_interface()	{ return G_TYPE_INTERFACE; }
-GType _g_type_char()		{ return G_TYPE_CHAR; }
-GType _g_type_uchar()		{ return G_TYPE_UCHAR; }
-GType _g_type_boolean()		{ return G_TYPE_BOOLEAN; }
-GType _g_type_int()		{ return G_TYPE_INT; }
-GType _g_type_uint()		{ return G_TYPE_UINT; }
-GType _g_type_long()		{ return G_TYPE_LONG; }
-GType _g_type_ulong()		{ return G_TYPE_ULONG; }
-GType _g_type_int64()		{ return G_TYPE_INT64; }
-GType _g_type_uint64()		{ return G_TYPE_UINT64; }
-GType _g_type_enum()		{ return G_TYPE_ENUM; }
-GType _g_type_flags()		{ return G_TYPE_FLAGS; }
-GType _g_type_float()		{ return G_TYPE_FLOAT; }
-GType _g_type_double()		{ return G_TYPE_DOUBLE; }
-GType _g_type_string()		{ return G_TYPE_STRING; }
-GType _g_type_pointer()		{ return G_TYPE_POINTER; }
-GType _g_type_boxed()		{ return G_TYPE_BOXED; }
-GType _g_type_param()		{ return G_TYPE_PARAM; }
-GType _g_type_object()		{ return G_TYPE_OBJECT; }
-GType _g_type_gtype()		{ return G_TYPE_GTYPE; }
-GType _g_type_variant()		{ return G_TYPE_VARIANT; }
+GType _g_type_interface()		{ return G_TYPE_INTERFACE; }
+GType _g_type_char()			{ return G_TYPE_CHAR; }
+GType _g_type_uchar()			{ return G_TYPE_UCHAR; }
+GType _g_type_boolean()			{ return G_TYPE_BOOLEAN; }
+GType _g_type_int()			{ return G_TYPE_INT; }
+GType _g_type_uint()			{ return G_TYPE_UINT; }
+GType _g_type_long()			{ return G_TYPE_LONG; }
+GType _g_type_ulong()			{ return G_TYPE_ULONG; }
+GType _g_type_int64()			{ return G_TYPE_INT64; }
+GType _g_type_uint64()			{ return G_TYPE_UINT64; }
+GType _g_type_enum()			{ return G_TYPE_ENUM; }
+GType _g_type_flags()			{ return G_TYPE_FLAGS; }
+GType _g_type_float()			{ return G_TYPE_FLOAT; }
+GType _g_type_double()			{ return G_TYPE_DOUBLE; }
+GType _g_type_string()			{ return G_TYPE_STRING; }
+GType _g_type_pointer()			{ return G_TYPE_POINTER; }
+GType _g_type_boxed()			{ return G_TYPE_BOXED; }
+GType _g_type_param()			{ return G_TYPE_PARAM; }
+GType _g_type_object()			{ return G_TYPE_OBJECT; }
+GType _g_type_gtype()			{ return G_TYPE_GTYPE; }
+GType _g_type_variant()			{ return G_TYPE_VARIANT; }
+GType _g_type_param_boolean()		{ return G_TYPE_PARAM_BOOLEAN; }
+GType _g_type_param_boxed()		{ return G_TYPE_PARAM_BOXED; }
+GType _g_type_param_char()		{ return G_TYPE_PARAM_CHAR; }
+GType _g_type_param_double()		{ return G_TYPE_PARAM_DOUBLE; }
+GType _g_type_param_enum()		{ return G_TYPE_PARAM_ENUM; }
+GType _g_type_param_flags()		{ return G_TYPE_PARAM_FLAGS; }
+GType _g_type_param_float()		{ return G_TYPE_PARAM_FLOAT; }
+GType _g_type_param_gtype()		{ return G_TYPE_PARAM_GTYPE; }
+GType _g_type_param_int()		{ return G_TYPE_PARAM_INT; }
+GType _g_type_param_int64()		{ return G_TYPE_PARAM_INT64; }
+GType _g_type_param_long()		{ return G_TYPE_PARAM_LONG; }
+GType _g_type_param_object()		{ return G_TYPE_PARAM_OBJECT; }
+GType _g_type_param_override()		{ return G_TYPE_PARAM_OVERRIDE; }
+GType _g_type_param_param()		{ return G_TYPE_PARAM_PARAM; }
+GType _g_type_param_pointer()		{ return G_TYPE_PARAM_POINTER; }
+GType _g_type_param_string()		{ return G_TYPE_PARAM_STRING; }
+GType _g_type_param_uchar()		{ return G_TYPE_PARAM_UCHAR; }
+GType _g_type_param_uint()		{ return G_TYPE_PARAM_UINT; }
+GType _g_type_param_uint64()		{ return G_TYPE_PARAM_UINT64; }
+GType _g_type_param_ulong()		{ return G_TYPE_PARAM_ULONG; }
+GType _g_type_param_unichar()		{ return G_TYPE_PARAM_UNICHAR; }
+GType _g_type_param_value_array()	{ return G_TYPE_PARAM_VALUE_ARRAY; }
+GType _g_type_param_variant()		{ return G_TYPE_PARAM_VARIANT; }
+
+GParamSpec *_g_object_find_property(GObject *object, const char *name)
+{
+	GObjectClass *cls = G_OBJECT_GET_CLASS(object);
+	return g_object_class_find_property(cls, name);
+}
 
 extern void *g_go_interface_copy_go(void *boxed);
 extern void g_go_interface_free_go(void *boxed);
--- gobject-2.0/gobject.h
+++ gobject-2.0/gobject.h
@@ -2,6 +2,8 @@
 
 typedef struct _GGoClosure GGoClosure;
 
+GType _g_param_spec_type(GParamSpec *pspec);
+GType _g_param_spec_value_type(GParamSpec *pspec);
 GType _g_object_type(GObject *obj);
 GType _g_value_type(GValue *val);
 GType _g_type_interface();
@@ -26,6 +28,31 @@ GType _g_type_object();
 GType _g_type_gtype();
 GType _g_type_variant();
 GType _g_type_go_interface();
+GType _g_type_param_boolean();
+GType _g_type_param_boxed();
+GType _g_type_param_char();
+GType _g_type_param_double();
+GType _g_type_param_enum();
+GType _g_type_param_flags();
+GType _g_type_param_float();
+GType _g_type_param_gtype();
+GType _g_type_param_int();
+GType _g_type_param_int64();
+GType _g_type_param_long();
+GType _g_type_param_object();
+GType _g_type_param_override();
+GType _g_type_param_param();
+GType _g_type_param_pointer();
+GType _g_type_param_string();
+GType _g_type_param_uchar();
+GType _g_type_param_uint();
+GType _g_type_param_uint64();
+GType _g_type_param_ulong();
+GType _g_type_param_unichar();
+GType _g_type_param_value_array();
+GType _g_type_param_variant();
+
+GParamSpec *_g_object_find_property(GObject *object, const char *name);
 
 GGoClosure *g_goclosure_new(int32_t id, void **iface);
 int32_t g_goclosure_get_id(GGoClosure *clo);
