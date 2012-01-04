42095757e96578887b2ef60464714c1e852c6012
nsf <no.smile.face@gmail.com>
2012-01-04 19:02:22 +0600

    Add Entry Buffer demo.

--- gogtk-demo/demos.go
+++ gogtk-demo/demos.go
@@ -9,6 +9,7 @@ import (
 	"./expander"
 	"./color_selector"
 	"./info_bar"
+	"./entry_buffer"
 )
 
 type DemoFunc func(mainwin *gtk.Window) *gtk.Window
@@ -26,8 +27,11 @@ var demos = []*DemoDesc{
 	{Title: "Spinner",        Filename: "spinner.go",        Func: spinner.Do},
 	{Title: "Expander",       Filename: "expander.go",       Func: expander.Do},
 	{Title: "Tree View", Children: []*DemoDesc{
-		{Title: "List Store",     Filename: "list_store.go",     Func: list_store.Do},
+		{Title: "List Store", Filename: "list_store.go", Func: list_store.Do},
 	}},
 	{Title: "Color Selector", Filename: "color_selector.go", Func: color_selector.Do},
 	{Title: "Info bar",       Filename: "info_bar.go",       Func: info_bar.Do},
+	{Title: "Entry", Children: []*DemoDesc{
+		{Title: "Entry Buffer", Filename: "entry_buffer.go", Func: entry_buffer.Do},
+	}},
 }
