f3198c28d8fe1d78882ea64ece1c8fff10896688
nsf <no.smile.face@gmail.com>
2012-01-07 21:44:17 +0600

    Sort demos.

--- gogtk-demo/demos.go
+++ gogtk-demo/demos.go
@@ -27,17 +27,17 @@ type DemoDesc struct {
 var demos = []*DemoDesc{
 	{Title: "Application main window", Filename: "main_window.go",    Func: main_window.Do},
 	{Title: "Button Boxes",            Filename: "button_boxes.go",   Func: button_boxes.Do},
-	{Title: "Links",                   Filename: "links.go",          Func: links.Do},
-	{Title: "Spinner",                 Filename: "spinner.go",        Func: spinner.Do},
-	{Title: "Expander",                Filename: "expander.go",       Func: expander.Do},
-	{Title: "Tree View", Children: []*DemoDesc{
-		{Title: "List Store", Filename: "list_store.go", Func: list_store.Do},
-	}},
 	{Title: "Color Selector",          Filename: "color_selector.go", Func: color_selector.Do},
-	{Title: "Info bar",                Filename: "info_bar.go",       Func: info_bar.Do},
 	{Title: "Entry", Children: []*DemoDesc{
 		{Title: "Entry Buffer", Filename: "entry_buffer.go", Func: entry_buffer.Do},
 	}},
-	{Title: "Pickers",                 Filename: "pickers.go",        Func: pickers.Do},
+	{Title: "Expander",                Filename: "expander.go",       Func: expander.Do},
+	{Title: "Info bar",                Filename: "info_bar.go",       Func: info_bar.Do},
+	{Title: "Links",                   Filename: "links.go",          Func: links.Do},
 	{Title: "Paned Widgets",           Filename: "paned_widgets.go",  Func: paned_widgets.Do},
+	{Title: "Pickers",                 Filename: "pickers.go",        Func: pickers.Do},
+	{Title: "Spinner",                 Filename: "spinner.go",        Func: spinner.Do},
+	{Title: "Tree View", Children: []*DemoDesc{
+		{Title: "List Store", Filename: "list_store.go", Func: list_store.Do},
+	}},
 }
