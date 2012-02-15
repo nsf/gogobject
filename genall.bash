2bb7e65528ffbc2826f63fbeb9eee5818275628d
Utkan Güngördü <utkan@freeconsole.org>
2012-02-16 06:38:48 +0900

    delete cairo-1.0 from genall.bash

--- genall.bash
+++ genall.bash
@@ -1,6 +1,6 @@
 #/bin/bash
 
-for pkg in glib-2.0 gobject-2.0 cairo-1.0 atk-1.0 gio-2.0 gdkpixbuf-2.0 pango-1.0 pangocairo-1.0 gdk-2.0 gtk-2.0 gdk-3.0 gtk-3.0 gtksource-3.0
+for pkg in glib-2.0 gobject-2.0 atk-1.0 gio-2.0 gdkpixbuf-2.0 pango-1.0 pangocairo-1.0 gdk-2.0 gtk-2.0 gdk-3.0 gtk-3.0 gtksource-3.0
 do
 	./gogobject -config config.json $pkg
 done
