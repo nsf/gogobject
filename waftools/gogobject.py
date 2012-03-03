f738f9d2a53b54166cbd0b527ae43dea89c12e57
nsf <no.smile.face@gmail.com>
2012-03-03 10:45:00 +0600

    Add go-gobject-gen binary as a dependency to all gogobject tasks.

--- waftools/gogobject.py
+++ waftools/gogobject.py
@@ -6,13 +6,15 @@ class gogobject(Task):
 
 @extension('.go.in')
 def gogobject_hook(self, node):
+	tg = self.bld.get_tgen_by_name('go-gobject-gen')
+	ggg = tg.link_task.outputs[0]
+
 	if not self.env.GGG:
-		tg = self.bld.get_tgen_by_name('go-gobject-gen')
-		ggg = tg.link_task.outputs[0]
 		self.env.GGG = ggg.abspath()
 
 	go_out = node.change_ext('')
 	c_out = go_out.change_ext('.gen.c')
 	h_out = go_out.change_ext('.gen.h')
 	task = self.create_task('gogobject', node, [go_out, c_out, h_out])
+	task.dep_nodes = [ggg]
 	return task
